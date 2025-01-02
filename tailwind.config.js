79288902453858d2e65cf11152984403c2094118
Aravind G <aravindg1@iisc.ac.in>
2025-01-02 13:19:02 +0530

    install tailwid

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+    './old/**/*.html',
+    './old/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
